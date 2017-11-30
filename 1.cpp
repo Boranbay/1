802649486bc9dbab3bea00150f54a7483a3f12b1
Boranbay <zhan1399@gmail.com>
2017-12-01 00:43:32 +0600

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,2 +1,18 @@
+// MS Visual 2017
+
+#include "stdafx.h"
 #include <iostream>
+#include <conio.h>
 #include <string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "Atyndy zhaz: ";
+	cin >> name;
+	cout << "Salem ," << name << "!";
+	getch();
+    return 0;
+}
